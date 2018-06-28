7d996114b17e73a5f5f32cfbc659707c76d498f7
susham <susham.kumar@gmail.com>
2018-06-28 09:51:09 -0700

    Pricing Schema modification

--- api/models/pricing_model.js
+++ api/models/pricing_model.js
@@ -10,40 +10,47 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let priceSchema = new Schema({
+   //Combination of time_slot and the created_on will give the price details for that particular day
     time_slot: {
-        type: String,
-        required: true,
-    },
-    total_price: {
         type: Number,
-        min: 0,
-        // required: true,  // TODO: likely calculated on the backend.
-    },
-    job_id: {
-        type: mongoose.Schema.Types.ObjectId,
         required: true,
     },
-    prices_per_hour: {
-        cpus: {
-            type: Number,
-            required: true,
-            min: 0,
-        },
-        gpus: {
-            type: Number,
-            required: true,
-            min: 0,
-        },
-        memory: {
-            type: Number,
-            required: true,
-            min: 0,
-        },
-        disk_space: {
-            type: Number,
-            required: true,
-            min: 0,
-        }
+    cpus: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    gpus: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    memory: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    disk_space: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    created_on: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    updated_on: {
+        type: Date,
+        default: Date.now
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    updated_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
     }
 });
 
