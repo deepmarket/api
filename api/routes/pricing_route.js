da7728d93fec517fb5d604e50855a58158551887
samgomena <samiam4612@gmail.com>
2018-07-13 14:45:47 -0700

    fix: and some more fixes.

--- api/routes/pricing_route.js
+++ api/routes/pricing_route.js
@@ -7,9 +7,9 @@ let router = express.Router();
 let verifyToken = require("../controllers/verifyToken");
 let price_controller = require('../controllers/pricing_controller');
 
-router.get('/', price_controller);
+router.get('/', price_controller.get_prices);
 
-router.post('/:price_id', verifyToken, price_controller);
+router.post('/:price_id', verifyToken, price_controller.add_price);
 
 // router.put('/price_id', verifyToken, price_controller);
 
