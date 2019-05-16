9d3439dce5313e47406d69878ad62901dc856561
samgomena <samiam4612@gmail.com>
2019-05-15 20:50:04 -0700

    refac: Move controller namespace to account namespace.

--- api/routes/account_route.js
+++ api/routes/account_route.js
@@ -10,19 +10,19 @@ let router = require('express').Router();
 
 const config = require('../config/config.js');
 const verifyToken = require(`${config.CONTROLLERS_PATH}/verifyToken`);
-let customer_controller = require(`${config.CONTROLLERS_PATH}/customer_controller`);
+let account_controller = require(`${config.CONTROLLERS_PATH}/account_controller`);
 
 
 // Get a users account information
-router.get('/', verifyToken, customer_controller.get_account_by_id);
+router.get('/', verifyToken, account_controller.get_account_by_id);
 
 // Create a new account
-router.post('/', customer_controller.add_account);
+router.post('/', account_controller.add_account);
 
 // Update a user's account details
-router.put('/', verifyToken, customer_controller.update_account_by_id);
+router.put('/', verifyToken, account_controller.update_account_by_id);
 
 // Delete a user's account
-router.delete('/', verifyToken, customer_controller.delete_account_by_id);
+router.delete('/', verifyToken, account_controller.delete_account_by_id);
 
 module.exports = router;
