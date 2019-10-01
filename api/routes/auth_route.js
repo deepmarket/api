9ded4a4ff4e951d6ffb79f88c96d3f9b9cf9b01f
samgomena <samiam4612@gmail.com>
2019-09-30 21:32:16 -0700

    feat: Add endpoint for refreshing a token `/auth/refresh`; add iss and exp fields to jwt payload.

--- api/routes/auth_route.js
+++ api/routes/auth_route.js
@@ -7,9 +7,10 @@ const config = require('../config/config');
 const verifyToken = require(`${config.CONTROLLERS_PATH}/verifyToken`);
 const authController = require(`${config.CONTROLLERS_PATH}/auth_controller`);
 
-/* Create a new customer */
 router.post('/login', authController.login);
 
 router.post('/logout', verifyToken, authController.logout);
+
+router.post('/refresh', verifyToken, authController.refresh);
 
 module.exports = router;
